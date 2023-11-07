d06b46cb211bb829f0e94de3d975d2e8fc4b4280
abhishek kumar <abhishekkumar@abhisheks-MacBook-Air.local>
2023-11-07 23:38:24 +0530

    push all file

---
+++ src/menu-items/pages.js
@@ -0,0 +1,51 @@
+// assets
+import { IconKey } from '@tabler/icons';
+
+// constant
+const icons = {
+  IconKey
+};
+
+// ==============================|| EXTRA PAGES MENU ITEMS ||============================== //
+
+const pages = {
+  id: 'pages',
+  title: 'Pages',
+  caption: 'Pages Caption',
+  type: 'group',
+  children: [
+    {
+      id: 'authentication',
+      title: 'Authentication',
+      type: 'collapse',
+      icon: icons.IconKey,
+
+      children: [
+        {
+          id: 'login3',
+          title: 'Login',
+          type: 'item',
+          url: '/pages/login/login3',
+          target: true
+        },
+        {
+          id: 'register3',
+          title: 'Register',
+          type: 'item',
+          url: '/pages/register/register3',
+          target: true
+        }
+      ]
+    },
+    {
+      id: 'about',
+      title: 'About',
+      type: 'item',
+      url: '/About',
+      icon: icons.IconPalette,
+      breadcrumbs: false
+    },
+  ]
+};
+
+export default pages;
